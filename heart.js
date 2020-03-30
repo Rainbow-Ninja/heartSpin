4cbb4a5ad0c8755d0a1fe81fdcd7aa197d2516a4
Jo Dunham <scaffelpike@yahoo.com.au>
2020-03-30 21:54:34 +1100

    first commit

---
+++ heart.js
@@ -0,0 +1,17 @@
+function expandHeart(x){
+    x.style.height = "30%";
+    x.style.width = "30%";
+}
+
+function normalHeart(x){
+    x.style.height = "15%";
+    x.style.width = "15%";
+}
+
+function backgroundBit(x){
+    x.style.background = "conic-gradient(navy 0 10%, green 0 20%, lime 0 30%, yellow 0 40%, orange 0 50%, rgb(250, 100, 0) 0 60%, red 0 70%, purple 0 80%, rgb(174, 1, 184) 0 90%, rgb(1, 1, 204) 0 100%)";
+}
+
+function unbackgroundBit(x){
+    x.style.background = "white";
+}
